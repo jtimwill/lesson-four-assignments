7eff209d483cbed49a90d85ca2b1b7b0b6f804ad
jtimwill <jtimwill@gmail.com>
2015-02-24 21:14:20 -0500

    Update application.js

--- tl-blackjack-webapp-jtw/public/application.js
+++ tl-blackjack-webapp-jtw/public/application.js
@@ -1,33 +1,22 @@
 // Tim Williams 2/23/2015
 // This code was created using the tutorials at gotealeaf.com
  
- $(document).ready(function(){  //anonymous function (no name)
-  //note: ready means do this once (when the document loads)
-//   $('#player_color').click(function(){
-//      alert('hi!');
-//      // $('player_area').css('background-color', 'green')
-//      return false;
-//   });
+ $(document).ready(function(){  
   player_hits();
   player_stay();
   dealer_hit();
 });
 
 function player_hits() {
-  //$('#hit_form input').click(function(){  //<-- binds only once to header
   $(document).on('click', 'form#hit_form input', function() { //<--rebinds
       //ajax request
       $.ajax({
         type: 'POST',
         url: '/game/player/hit'
-        //data: {} //you don't need additonal paramenters in this case
       }).done(function(msg){
-        //alert(msg); //done takes a message as a parameter
-
-        //below: only return the 'game' div. not everything
-        $('div#game').replaceWith(msg);//substitute whole element, not just the contents
+        $('div#game').replaceWith(msg);
       });
-      return false; //supress the execution of the form so that we can hi-jack it
+      return false;
     });
 }
 
@@ -54,16 +43,3 @@ function dealer_hit() {
     return false;
   });
 }
-
-
-//note: you could do above with CSS, but you would be hiiting the server 
-//each time you click
-
-//you can use consol.log() if you are debugging with firefox
-
-/*
-webpage loads
-browser then fires an event
-jquery then picks up that event
-jquery runs code to binds element continously (expensive, you should probably use angular)
-*/
